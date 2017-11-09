0418785071e932e51b6d6c675309c15ef65e573f
Fadhil Imam Kurnia <fadhilimamk@gmail.com>
2017-11-09 21:54:07 +0700

    use heroku port from env variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Creating App instance
 const express = require('express');
 const app = express();
+app.set('port', (process.env.PORT || 5000));
 
 // Main handler
 app.get('/', function(request, response) {
@@ -12,7 +13,6 @@ app.post('/', function(request, response) {
 });
 
 // Start server
-app.listen(
-    3000, 
-    () => console.log('Bang Teti is listening on port 3000!')
-);
+app.listen(app.get('port'), function() {
+  console.log('Bang Teti is listening on port', app.get('port'));
+});
