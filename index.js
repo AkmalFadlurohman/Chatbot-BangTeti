1b32793c677e8e89bebf316e94c28b121be8992e
Irene Edria <ireneedriadr@gmail.com>
2017-11-16 13:58:46 +0700

    imagemap

--- index.js
+++ index.js
@@ -179,8 +179,8 @@ function handleHelp(replyToken) {
 
 function handleTop10(replyToken) {
     var reply = {
-          type: "imagemap", 
-          [{
+          type: "imagemap",
+          imagemap: [{
               baseUrl: "https://img.okezone.com/content/2017/10/03/33/1787616/pasrah-jeremy-teti-mengaku-kesulitan-mencari-jodoh-C1LQd3TusT.jpg/520",
               altText: "Bang Teti ngirim Top-10 nih",
               baseSize: {
