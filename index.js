a407c63a294065d209f5a0eff2dc0bd52928c99d
Wenny Yustalim <wennyyustalim@gmail.com>
2017-11-18 11:42:19 +0700

    test buttons

--- index.js
+++ index.js
@@ -225,6 +225,37 @@ function handleError(replyToken) {
     });;
 }
 
+function handleFeedback(replyToken) {
+    console.log("\tBang Teti asks for feedback.");
+    const reply = {
+      "type": "template",
+      "altText": "this is a buttons template",
+      "template": {
+          "type": "buttons",
+          "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+          "title": "Menu",
+          "text": "Please select",
+          "actions": [
+              {
+                "type": "postback",
+                "label": "Buy",
+                "data": "action=buy&itemid=123"
+              },
+              {
+                "type": "postback",
+                "label": "Add to cart",
+                "data": "action=add&itemid=123"
+              },
+              {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/123"
+              }
+          ]
+      }
+}
+}
+
 // Start server
 app.listen(app.get('port'), function() {
     console.log('Bang Teti is listening on port', app.get('port'));
